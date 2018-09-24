da4b5d0bb42f958394cf881b58a79caed2fcec49
ngalecki <ngalecki@purdue.edu>
2018-09-23 22:17:29 -0400

    Added UserPassword Class

---
+++ src/UserPassword.java
@@ -0,0 +1,11 @@
+
+public class UserPassword {
+	private int UserID;
+	private String Salt;
+	private String SaltedPassword;
+	
+	// Returns true on successful login, false on unsuccessful login
+	public static boolean IsPasswordCorrect(String userName, String password){
+        return false;
+	}
+}
