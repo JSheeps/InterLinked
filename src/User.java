7d2c27accf0cbea5d22146a50bf0297e85979241
ngalecki <ngalecki@purdue.edu>
2018-09-30 21:50:46 -0400

    Added SQL to User Class

--- src/User.java
+++ src/User.java
@@ -1,27 +1,39 @@
 import com.sun.media.jfxmedia.events.PlayerStateListener;
 
 import java.util.List;
+import java.sql.*;
 
 public class User {
-
-    public User(String userName, String password, String email){
-        this.userName = userName;
-        this.email = email;
-
-        // TODO save to db
-
-        // Fetch ID back, make UserPassword instance
-
-        // TODO Dummy ID for now
-        this.ID = 1;
-
-        UserPassword userPassword = new UserPassword(this.ID, password);
-    }
 
     int ID;
     String userName;
     String email;
     List<Playlist> playlistList;
 
+    public User(String userName, String password, String email){
+        this.userName = userName;
+        this.email = email;
 
+        java.util.Date currentDate = new java.util.Date();
+        Date sqlDate = new Date(currentDate.getTime());
+
+        String insertion = "INSERT INTO Users([UserName], [Email], [CreatedDate])" +
+                           "VALUES(" + this.userName + ", " + this.email + ", " + sqlDate + ")";
+
+        SqlHelper helper = new SqlHelper();
+
+        helper.ExecuteQuery(insertion);
+
+        // Fetch ID back, make UserPassword instance
+        String idFetchQuery = "SELECT ID FROM Users WHERE UserName = " + this.userName;
+        ResultSet results = helper.ExecuteQuery(idFetchQuery);
+
+        try{
+            this.ID = results.getInt("ID");
+        }catch(SQLException e){
+            // TODO
+        }
+
+        UserPassword userPassword = new UserPassword(this.ID, password);
+    }
 }
