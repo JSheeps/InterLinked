ceb1cd4b639fc40abbf5eb784180e64d1b2f08dd
ngalecki <ngalecki@purdue.edu>
2018-09-30 21:35:38 -0400

    Added UnitTests Class, added User Creation Test

---
+++ src/UnitTests.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+import org.junit.runner.JUnitCore;
+
+public class UnitTests {
+
+    public static void main(String[] args){
+        UserCreationTest1();
+    }
+
+    @Test(timeout = 100)
+    public static void UserCreationTest1(){
+        String userName = "testUserName";
+        String password = "testPassword";
+        String email = "testEmail@test.com";
+
+        User user = new User(userName, password, email);
+
+        boolean actual = UserPassword.IsPasswordCorrect(userName, password);
+
+        boolean expected = true;
+
+        SqlHelper helper = new SqlHelper();
+        String deletionQuery = "DELETE FROM UserPasswords WHERE UserID = " + user.ID;
+        String deletionQuery2 = "DELETE FROM Users WHERE ID = " + user.ID;
+
+        helper.ExecuteQuery(deletionQuery);
+        helper.ExecuteQuery(deletionQuery2);
+
+        Assert.assertEquals(expected, actual);
+    }
+}
