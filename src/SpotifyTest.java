26b827603103c44fb503002f534205b8923ac190
Doug York <dyork367@gmail.com>
2018-09-30 23:28:09 -0400

    Update SpotifyTest.java

--- src/SpotifyTest.java
+++ src/SpotifyTest.java
@@ -3,9 +3,7 @@ import java.net.*;
 import com.wrapper.spotify.*;
 
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-/**
- * Created by Douglas on 9/22/2018.
- */
+
 public class SpotifyTest {
     public static void main(String[] args) {
         Spotify s = new Spotify();
