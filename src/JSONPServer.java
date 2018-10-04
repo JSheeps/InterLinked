6c45701177d7da120236375d717c1f685008def5
Andrew Furman <andrew@efurman.com>
2018-10-03 21:43:05 -0400

    Further implimented server
    Now tracks active users, allows for signup and signin, and gets playlist for current user.

--- src/JSONPServer.java
+++ src/JSONPServer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Executor;
 
@@ -23,6 +23,7 @@ public class JSONPServer {
             server.setExecutor(new HttpThreadCreator());
 
             server.start();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -41,10 +42,14 @@ class HttpThreadCreator implements Executor {
 // How to handle calls to the /data endpoint
 class DataHandler implements HttpHandler {
 
+    // List of currently logged in users and their socket addresses
+    HashMap<InetSocketAddress, User> activeUsers;
+
     private static final int UNPROCESSABLE_ENTITY = 422;
     private static final int GOOD = 200;
 
     public void handle(HttpExchange t) throws IOException {
+
         Headers headers = t.getResponseHeaders();
 
         // Necessary for JSONP
@@ -59,18 +64,18 @@ class DataHandler implements HttpHandler {
             return;
         }
 
-        JSONObject jsonReturn;
+        JSONObject json;
 
         // Process Query into JSON
         try {
-            jsonReturn = commandRedirect(query);
+            json = commandRedirect(query, t.getRemoteAddress());
         } catch (Exception e) {
             badQuery(t, e.getMessage());
             return;
         }
 
         // Format JSON into http response
-        String response = getJSONPMessage(jsonReturn, callback);
+        String response = getJSONPMessage(json, callback);
         t.sendResponseHeaders(GOOD, response.length());
 
         try (OutputStream os = t.getResponseBody()) {
@@ -81,26 +86,74 @@ class DataHandler implements HttpHandler {
     }
 
     // Expand this for queries
-    private JSONObject commandRedirect(QueryValues query) throws Exception {
+    private JSONObject commandRedirect(QueryValues query, InetSocketAddress remoteAddress) throws Exception {
 
         //TODO Implement more query commands
 
-        if (query.containsKey("example")) {
-        } else if (query.containsKey("get")) {
-            return get(query);
-        }
+        boolean isLoggedIn = activeUsers.containsKey(remoteAddress);
+
+        if (query.containsKey("get")) {
+
+            if(isLoggedIn)
+                return get(activeUsers.get(remoteAddress));
+            else
+                throw new Exception("User needs to log in");
+
+        } else if (query.containsKey("signup"))
+            return signUp(query);
+
+        else if (query.containsKey("login"))
+            return logIn(query, remoteAddress);
 
         throw new Exception("Query has no meaning");
     }
 
-    @SuppressWarnings("unchecked")
-    private JSONObject get(QueryValues query) {
+    // Method to handle "signup" query. Returns json with true on success and false on failure
+    private JSONObject signUp(QueryValues query) {
         JSONObject json = new JSONObject();
 
-        List<String> playlists = new ArrayList<>();
+        String encodedName = query.get("signup");
 
-        playlists.add("Example playlist 1");
-        playlists.add("Example playlist 2");
+        // Format expected is email:username:password
+        String[] info = encodedName.split(":", 3);
+
+        User user = User.CreateUser(info[1], info[3], info[0]);
+
+        json.put("result", user != null);
+
+        return json;
+    }
+
+    // Method to handle "login" query. Returns json with true on success and false on failure
+    private JSONObject logIn(QueryValues query, InetSocketAddress remoteAddress) {
+        JSONObject json = new JSONObject();
+
+        String encodedName = query.get("login");
+
+        // Expected format is "username:password"
+        String[] info = encodedName.split(":", 2);
+
+        boolean b = UserPassword.IsPasswordCorrect(info[0], info[1]);
+
+        if(b){
+            User user = User.getUserByUserName(info[0]);
+
+            if(user != null)
+                activeUsers.put(remoteAddress, user);
+            else
+                b = false;
+        }
+
+        json.put("result", b);
+        return json;
+    }
+
+    // Method to handle "get" query. Returns json with a list of playlist objects for current user
+    private JSONObject get(User user) {
+        JSONObject json = new JSONObject();
+
+        user.FetchPlaylists();
+        List<Playlist> playlists = user.playlistList;
 
         json.put("playlists", playlists);
 
