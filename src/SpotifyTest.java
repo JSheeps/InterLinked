fb5b140930acd3b9ddcc109aed53878a76bcf2a4
ngalecki <ngalecki@purdue.edu>
2018-10-05 12:32:05 -0400

    Merge branch 'master' of C:\Users\obliv\IdeaProjects\InterLinked with conflicts.

--- src/SpotifyTest.java
+++ src/SpotifyTest.java
@@ -1,13 +1,13 @@
-import java.util.*;
-import java.net.*;
-import com.wrapper.spotify.*;
-
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
-
-public class SpotifyTest {
-    public static void main(String[] args) {
-        Spotify s = new Spotify();
-        s.Login();
-        s.getPlaylists();
-    }
-}
+//import java.util.*;
+//import java.net.*;
+//import com.wrapper.spotify.*;
+//
+//import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+//
+//public class SpotifyTest {
+//    public static void main(String[] args) {
+//        Spotify s = new Spotify();
+//        s.Login();
+//        s.getPlaylists();
+//    }
+//}
