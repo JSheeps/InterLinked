6e3c270648af5174af3869f9192bd865924d03ce
JSheeps <joshshepler@msn.com>
2018-10-28 20:50:50 -0400

    Added Share and Remove links in the My Playlists table

--- CS307 Front-end/Main Page/playlists.js
+++ CS307 Front-end/Main Page/playlists.js
@@ -18,12 +18,6 @@ function viewPlaylists(tableSelector) {
 	tableText(table, "Loading...");
 	
 	getPlaylistsFromServer().done( (playlists) => {
-		/*playlists = [
-			{ name: "test" },
-			{ name: "second" },
-			{ name: "third" },
-			{ name: "fourth" }
-		];*/
 		table = clearTable(tableSelector);
 		if (playlists.error) {
 			if (playlists.error == "NotLoggedInToService: User needs to log in to streaming service") {
@@ -39,59 +33,16 @@ function viewPlaylists(tableSelector) {
 		}
 		
 		console.log(playlists);
-		var html = "";
-		html += "<tr class='sorter'><td onclick='sort(0, \"" + tableSelector + "\")' class='clickable'><u>"
-		html += "Playlist Names";
-		html += "</u></td></tr>";
-		
 		for (var i = playlists.length - 1; i >= 0; i--) {
+			var str = "<tr>"
 			var playlist = playlists[i];
-			var str = "<tr><td sortData='" + playlist.name + "'>" + playlist.name + "</td></tr>";
-			html += str;
+			str+="<td><a class='black' onclick=\"sharePlayList();\">Share</a></td>"
+			str+="<td>" + playlist.name + "</td>";
+			str+="<td><a class='black' onclick=\"removePlayList('" + playlist.id + "');\">Remove</a></td>"
+			str+="</tr>"
+			table = table.after(str);
 		}
-		
-		table.after(html);
 	});
-}
-
-function compare(a, b, ascending) {
-	if (a == b) return 0;
-	if (a < b) {
-		return ascending ? -1 : 1;
-	}
-	return ascending ? 1 : -1;
-}
-
-var orderBy;
-function sort(column, id) {
-	var table = $(id);
-	var tbody = table.children();
-	var rows = tbody.children();
-	
-	var ascending;
-	if (orderBy == column) {
-		var ascending = tbody.attr("order");
-		if (ascending == undefined)
-			ascending = true;
-		else if (ascending == "asc")
-			ascending = false;
-		else
-			ascending = true;
-	} else {
-		orderBy = column;
-		ascending = true;
-	}
-	console.log(ascending);
-	
-	tbody.find("tr:gt(1)").sort( (a, b) => {
-		return compare(
-			$(a).children().eq(column).attr("sortData"),
-			$(b).children().eq(column).attr("sortData"),
-			ascending
-		);			
-	}).appendTo(tbody);
-	
-	tbody.attr("order", (ascending ? "asc" : "desc"));
 }
 
 function tableText(table, text) {
@@ -127,7 +78,7 @@ function search() {
 				searchResults.append("<p class='black'>" + resultString + "</p>");
 				
 				if (result.SpotifyURL) {
-					searchResults.append("<a class='black' target='_blank' href=" + result.SpotifyURL + ">Spotify</a>");
+					searchResults.append("<a class='black' href=" + result.SpotifyURL + ">Spotify</a>");
 				}
 			}
 			
